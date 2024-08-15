903f432a75bcd0e40aab44177f76c3889f371f02
Rishabh Munjal <rishabh5566ind1111@gmail.com>
2024-08-16 02:16:55 +0530

    Task Manager UI

--- src/script.js
+++ src/script.js
@@ -1,11 +1,7 @@
-let video_itr = [1 , 2 , 3 , 4 ,5];
+const video_itr = [1 , 2 , 3 , 4 ,5];
 let i = 0;
 
-
-
 function ChangeVideo(){
-
-    
     const video = document.getElementById('background-video');
     const source = video.querySelector('source');
     const newVideoSource = `/assets/${video_itr[i]}.mp4`;
