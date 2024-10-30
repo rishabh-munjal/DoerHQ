73b3d359e6ab1989610e04f53c849f2687a767cf
Rishabh Munjal <rishabh5566ind1111@gmail.com>
2024-10-31 01:28:13 +0530

    Chatbot

--- README.md
+++ README.md
@@ -4,4 +4,11 @@ DoerHQ is a simple To-Do daily task manager website Build with Tailwind CSS &amp
 
 Todo:-
 1. time based changing background
+2. background
+3. todo memory
+4. reponsilveness
+5. scrollbar desing
+6. preloader
+7. deployment
+8. API ENV
 
--- src/chatbot.js
+++ src/chatbot.js
@@ -1,10 +1,17 @@
 var question = document.querySelector('.gemini-input');
 var sendButton = document.querySelector('#send-btn');
+var chatboxContainer = document.querySelector('#chat-display');
 
 const API = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyDNT77nL8WZhzeaMvLWTuWfQ-D9auG70I0';
 
 sendButton.addEventListener('click', function(e) {
     e.preventDefault();
+    
+    // Display the user's question in the chatbox
+    let chat = document.createElement('div');
+    chat.innerHTML = `<div class="text-sm bg-orange-400 w-[10rem] bg-opacity-50 p-2 rounded-md max-w-xs self-start">${question.value}</div>`;
+    chatboxContainer.appendChild(chat);
+
     getResponse();
     question.value = '';
 });
@@ -28,8 +35,33 @@ async function getResponse() {
         });
         
         const data = await response.json();
-        console.log(data.candidates[0].content.parts[0].text);  // Process or display the response as needed
+        let res = data.candidates[0].content.parts[0].text;
+        let res2 = jsonEsc(res);
+
+        let res3 = res2.split("**");
+        let res4 = "";
+
+        for(let i = 0 ; i < res3.length ; i++){
+            if(i == 0 || i%2 != 1){
+                res4 += res3[i];
+            }
+            else{
+                res4+="<strong>"+res3[i]+"</strong>";
+            }
+        }
+        
+        // Display the API's response in the chatbox
+        let chat2 = document.createElement('div');
+        chat2.innerHTML = `<div class="text-sm bg-white ml-7 w-[12rem] bg-opacity-20 p-2 rounded-md max-w-xs self-start">${res4}</div>`;
+        chatboxContainer.appendChild(chat2);
+
     } catch (error) {
         console.error('Error fetching response:', error);
     }
+}
+
+function jsonEsc(str){
+    return str
+    .replace(new RegExp("\r>\n\n" , "g") , "<br>")
+    .replace(new RegExp("\r?\n" , "g") , "<br>");
 }
